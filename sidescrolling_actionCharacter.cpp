536c25dfacdb97f9cfab827d3831c49eafd8668f
adavila0703 <36370758+adavila0703@users.noreply.github.com>
2019-11-21 07:33:32 -0600

    Update sidescrolling_actionCharacter.cpp

--- sidescrolling_actionCharacter.cpp
+++ sidescrolling_actionCharacter.cpp
@@ -1,6 +1,3 @@
-// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
-
-
 #include "sidescrolling_actionCharacter.h"
 #include "Camera/CameraComponent.h"
 #include "Components/CapsuleComponent.h"
